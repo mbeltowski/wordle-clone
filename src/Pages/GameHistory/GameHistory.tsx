76adfe7eb67f32776a826b496172baae272ece3f
mbeltowski <marcel.beltowski@gmail.com>
2023-06-17 16:26:28 +0200

    finish game history design

--- src/Pages/GameHistory/GameHistory.tsx
+++ src/Pages/GameHistory/GameHistory.tsx
@@ -1,4 +1,5 @@
-import QuestionMarkOutlinedIcon from "@mui/icons-material/QuestionMarkOutlined"
+import EventIcon from "@mui/icons-material/Event"
+import RestartAltIcon from "@mui/icons-material/RestartAlt"
 import { Avatar, Box, Grid } from "@mui/material"
 import GamesHistory from "Config/Models/GamesHistory"
 import { useEffect, useState } from "react"
@@ -25,15 +26,20 @@ const GameHistory = () => {
 			<Grid item container spacing={2} columnSpacing={{ xs: 2, sm: 6, md: 1 }}>
 				{history.length > 0 &&
 					history.map((gameFromHistory) => (
-						<Grid item className='history__container__games' xs={6} sm={6} md={3} lg={2.4} xl={2}>
+						<Grid item className='history__container__games' xs={12} sm={6} md={4} lg={3} xl={2}>
 							<div className='history__container__single-game'>
 								<div className='history__container__single-game__header'>
-									<Avatar>S+</Avatar>
+									<Avatar>S</Avatar>
 									<p>{gameFromHistory.game.word}yyy</p>
+									<div className='history__container__single-game__header__game-state'>win!</div>
 								</div>
 								<div className='history__container__single-game__other-data'>
-									<div className='history__container__single-game__tries'>
-										<QuestionMarkOutlinedIcon />
+									<div className='history__container__single-game__date' title='Game date'>
+										<EventIcon sx={{ fontSize: 22 }} />
+										12.08.2023
+									</div>
+									<div className='history__container__single-game__tries' title='Number of tries'>
+										<RestartAltIcon sx={{ fontSize: 22 }} />
 										{gameFromHistory.lastWordAttempts}
 									</div>
 								</div>
